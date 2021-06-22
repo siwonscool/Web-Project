6784e5ad76a72d7c42639f88f6ea73b5ff799516
한국직업전문학교 <@DESKTOP-LHHIDOO>
2021-06-22 09:05:01 +0900

    jsp 기초

---
+++ web0621/src/main/java/com/site/ex/Ex0621_01.java
@@ -0,0 +1,23 @@
+package com.site.ex;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/ex01")
+public class Ex0621_01 extends HttpServlet {
+	
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("ex01페이지가 접속이 되었습니다.");
+		
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	}
+
+}
